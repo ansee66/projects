18970cb59f7551b54bfd1d6607e1dd5e603fc399
Natalya Solovyova <ns-66@yandex.ru>
2022-10-27 12:40:35 +0500

    feat: add opportunity for choice of field size

--- gem-puzzle/index.js
+++ gem-puzzle/index.js
@@ -11,6 +11,9 @@ const scoreMoves = document.createElement('div');
 const scoreMovesCount = document.createElement('span');
 const gameField = document.createElement('div');
 const moveSound = new Audio('./assets/move.ogg');
+const settings = document.createElement('div');
+// const button3x3 = document.createElement('button');
+// const button4x4 = document.createElement('button');
 
 header.classList.add('header');
 restartButton.classList.add('button');
@@ -18,12 +21,14 @@ soundButton.classList.add('button');
 container.classList.add('container');
 score.classList.add('score');
 gameField.classList.add('game-field');
+settings.classList.add('settings');
 
 document.body.prepend(header, main);
 header.append(restartButton, soundButton);
 main.append(container);
-container.append(score, gameField);
+container.append(score, gameField, settings);
 score.append(scoreTimer, scoreMoves);
+// settings.append(button3x3, button4x4);
 
 restartButton.textContent = 'Shuffle and start';
 soundButton.textContent = 'Mute';
@@ -31,10 +36,13 @@ scoreTimer.textContent = '00:00:00';
 scoreMoves.textContent = 'Moves: ';
 scoreMoves.append(scoreMovesCount);
 scoreMovesCount.textContent = '0';
+// button3x3.textContent = '3x3';
+// button4x4.textContent = '4x4';
 
-const puzzle = new Puzzle(gameField, scoreTimer, scoreMovesCount, moveSound);
-puzzle.renderPuzzle();
-puzzle.shufflePuzzle();
+const puzzle = new Puzzle(gameField, scoreTimer, scoreMovesCount, moveSound, settings);
+puzzle.renderPuzzle(4);
+puzzle.renderSettings();
+puzzle.shufflePuzzle(4);
 puzzle.setHandlers();
 
 restartButton.addEventListener("click", () => {
