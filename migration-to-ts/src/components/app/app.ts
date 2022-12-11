b9409f8ec194e947059c0c6f4a3e95258dc65bfd
Natalya Solovyova <ns-66@yandex.ru>
2022-12-11 15:04:21 +0700

    refactor: replace app.js on .ts, add typing

---
+++ migration-to-ts/src/components/app/app.ts
@@ -0,0 +1,22 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+  controller: AppController;
+  view: AppView;
+
+  constructor() {
+    this.controller = new AppController();
+    this.view = new AppView();
+  }
+
+  start() {
+    const sourcesBlock = document.querySelector('.sources');
+    if (sourcesBlock instanceof HTMLElement) {
+      sourcesBlock.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+      this.controller.getSources((data) => this.view.drawSources(data));
+    }
+  }
+}
+
+export default App;
