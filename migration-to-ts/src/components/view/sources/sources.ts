eba175ee00aabd0b829761a17deb6dbc776c4793
Natalya Solovyova <ns-66@yandex.ru>
2022-12-11 12:11:23 +0700

    refactor: replace source.js on .ts, add typing, create interface for sources

---
+++ migration-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,35 @@
+import './sources.css';
+import { ISource } from '../../../types/index';
+
+class Sources {
+  draw(data: Array<ISource>) {
+    const fragment = document.createDocumentFragment();
+    const sourceItemTemp = document.querySelector('#sourceItemTemp');
+
+    if (sourceItemTemp instanceof HTMLTemplateElement) {
+      data.forEach((item) => {
+        const sourceClone = sourceItemTemp.content.cloneNode(true);
+        if (sourceClone instanceof DocumentFragment) {
+          const sourceItemName = sourceClone.querySelector('.source__item-name');
+          if (sourceItemName instanceof HTMLElement) {
+            sourceItemName.textContent = item.name;
+          }
+
+          const sourceItem = sourceClone.querySelector('.source__item');
+          if (sourceItem instanceof HTMLElement) {
+            sourceItem.setAttribute('data-source-id', item.id);
+          }
+
+          fragment.append(sourceClone);
+        }
+      });
+
+      const sources = document.querySelector('.sources');
+      if (sources instanceof HTMLElement) {
+        sources.append(fragment);
+      }
+    }
+  }
+}
+
+export default Sources;
--- migration-to-ts/src/types/index.ts
+++ migration-to-ts/src/types/index.ts
@@ -6,3 +6,13 @@ export enum Endpoint {
   Sources = 'sources',
   Everything = 'everything',
 }
+
+export interface ISource {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  category: string;
+  language: string;
+  country: string;
+}
