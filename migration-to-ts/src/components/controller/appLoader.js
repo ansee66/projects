2e1d4d4829ae9b9e24291adc55e98812cf596f53
Natalya Solovyova <ns-66@yandex.ru>
2022-12-11 09:12:03 +0700

    feat: add tsconfig.json, correct other config files

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'bddc22702afe4e8886647616e2cb31cf',
         });
     }
 }
