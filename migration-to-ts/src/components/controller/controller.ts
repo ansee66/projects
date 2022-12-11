75888aec9733a16d8d9aba1fd00b971febda00f3
Natalya Solovyova <ns-66@yandex.ru>
2022-12-11 15:00:34 +0700

    refactor: replace controller.js on .ts, add typing

---
+++ migration-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,49 @@
+import AppLoader from './appLoader';
+import { Endpoint, INews, ISources } from '../../types/index';
+
+class AppController extends AppLoader {
+  getSources(callback: (data?: ISources) => void) {
+    super.getResp(
+      {
+        endpoint: Endpoint.Sources,
+      },
+      callback
+    );
+  }
+
+  getNews(e: Event, callback: (data?: INews) => void) {
+    let target = e.target;
+    const newsContainer = e.currentTarget;
+    while (target !== newsContainer) {
+      if (target instanceof HTMLElement) {
+        if (target.classList.contains('source__item')) {
+          const sourceId = target.getAttribute('data-source-id');
+          if (newsContainer instanceof HTMLElement) {
+            if (newsContainer.getAttribute('data-source') !== sourceId) {
+              if (typeof sourceId === 'string') {
+                newsContainer.setAttribute('data-source', sourceId);
+                super.getResp(
+                  {
+                    endpoint: Endpoint.Everything,
+                    options: {
+                      sources: sourceId,
+                    },
+                  },
+                  callback
+                );
+              }
+            }
+          } else {
+            throw new Error('newsContainer is not HTMLElement');
+          }
+          return;
+        }
+        target = target.parentNode;
+      } else {
+        throw new Error('target is not HTMLElement');
+      }
+    }
+  }
+}
+
+export default AppController;
